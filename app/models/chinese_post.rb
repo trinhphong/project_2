3e253846acba127d61ce5a447d3a8c4ef9608ad0
trinhphong <nguyenquoctin19961996@gmail.com>
2017-12-04 18:54:17 +0700

    Add image for chinese post

--- app/models/chinese_post.rb
+++ app/models/chinese_post.rb
@@ -1,4 +1,9 @@
 class ChinesePost < ApplicationRecord
     has_many :chinese_phrases, dependent: :destroy
     belongs_to :admin, foreign_key: "admin_id"
+
+    has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+    validates :image, attachment_presence: true
+    validates_attachment :image, size: { in: 0..100.kilobytes }
+    validates_attachment :image, content_type: { content_type: "image/jpg" }
 end
