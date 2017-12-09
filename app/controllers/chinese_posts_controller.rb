cc0d8206a64de41fcab8083926377e67a326f949
trinhphong <nguyenquoctin19961996@gmail.com>
2017-12-09 11:39:51 +0700

    set up after sign in

--- app/controllers/chinese_posts_controller.rb
+++ app/controllers/chinese_posts_controller.rb
@@ -1,6 +1,6 @@
 class ChinesePostsController < ApplicationController
   before_action :authenticate_user!, only: [:show]
-  before_action :authenticate_admin!, only: [:create, :destroy, :update, :new]
+  before_action :authenticate_admin!, only: [:index, :create, :destroy, :update, :new]
   before_action :set_chinese_post, only: [:show, :edit, :update, :destroy]
 
   $translator = Yandex::Translator.new('trnsl.1.1.20171201T174723Z.ad1f067b0544dfc6.326ee4f45e33241a8a265b8d3424728de10618d4')
