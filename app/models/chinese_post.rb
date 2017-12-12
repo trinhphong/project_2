f1c599d1003e73978c78382ea0c1e4996cab8a93
tinnq <nguyenquoctin921996@gmail.com>
2017-12-13 00:26:06 +0700

    design more UI

--- app/models/chinese_post.rb
+++ app/models/chinese_post.rb
@@ -1,6 +1,7 @@
 class ChinesePost < ApplicationRecord
     has_many :chinese_phrases, dependent: :destroy
     has_many :vietnamese_phrases, through: :chinese_phrases
+    has_many :rates, through: :vietnamese_phrases
     belongs_to :user, foreign_key: "admin_id"
 
     has_attached_file :image, styles: {large: "500x200" }
